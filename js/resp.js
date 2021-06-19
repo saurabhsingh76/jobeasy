a67d77c8b13276ab6c62d0ecfe27ecf71e7ce996
saurabh singh <60366119+saurabhsingh76@users.noreply.github.com>
2021-06-19 23:28:14 +0530

    resposive_js_verion0

---
+++ js/resp.js
@@ -0,0 +1,31 @@
+window.onscroll = function() {sticky_navbar()};
+
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightNav = document.querySelector('.rightNav');
+container1margin = document.getElementById("container1-resp");
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    if(container1margin.style.marginTop=="217px"){
+        container1margin.style.marginTop="0px";   
+    }
+    else
+    {
+        container1margin.style.marginTop="217px";
+    }
+
+})
+
+var sticky = navbar.offsetTop;
+function sticky_navbar(){
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } 
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
