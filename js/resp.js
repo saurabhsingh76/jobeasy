303fc11af95b695e54f88a1ca1f2fa64b5d3ba16
saurabh singh <60366119+saurabhsingh76@users.noreply.github.com>
2021-06-22 00:47:52 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -1,33 +1,24 @@
-let is_scrolled = false;
-window.onscroll = function() {is_scrolled= true; sticky_navbar()};
+// window.onscroll = function() {sticky_navbar()};
 
 burger = document.querySelector('.burger');
 navbar = document.querySelector('.navbar');
 navlist = document.querySelector('.nav-list');
 rightNav = document.querySelector('.rightNav');
-container1margin = document.getElementById("container1-resp");
+// main_img = document.querySelector(".main-img");
 
 burger.addEventListener('click',()=>{
     rightNav.classList.toggle('v-class-resp');
     navlist.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-//     if(is_scrolled){
-//         if(container1margin.style.marginTop=="217px"){
-//             container1margin.style.marginTop="0px";   
-//         }
-//         else
-//         {
-//             container1margin.style.marginTop="217px";
-//         }
-//     }
 })
 
-var sticky = navbar.offsetTop;
-function sticky_navbar(){
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } 
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
+// var sticky = navbar.offsetTop;
+// function sticky_navbar(){
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } 
+//     else{
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
